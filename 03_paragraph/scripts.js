efba6ea33557de741ae6f9bcb979780c83e531f6
phillipskisshanee <phillips.kisshanee@wustl.edu>
2016-04-11 18:33:26 -0500

    Update scripts.js

---
+++ 03_paragraph/scripts.js
@@ -0,0 +1,5 @@
+alert("Help! I'm a fancy error message");
+alert("Pls fix me? I've fallen and I can't get up");
+document.write("<h2>My first JavaScript program</h2>");
+document.write("I'm practicing 'debugging'.");
+
