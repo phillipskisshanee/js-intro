b37de4f6272eb7e1db14c744f4a3068814df2e1a
phillipskisshanee <phillips.kisshanee@wustl.edu>
2016-04-11 18:30:31 -0500

    Update scripts.js

--- 02_files/scripts.js
+++ 02_files/scripts.js
@@ -1,4 +1,4 @@
-alert("Help me fix this program!");
-alert("Can you get this message to appear?");
+alert("Help! I'm a fancy error message");
+alert("Pls fix me? I've fallen and I can't get up");
 document.write("<h2>My first JavaScript program</h2>");
 document.write("I'm practicing 'debugging'.");
