60d4b24a68e007985ec58d2cef5ad9f42856e36b
phillipskisshanee <phillips.kisshanee@wustl.edu>
2016-04-11 18:48:35 -0500

    Update scripts.js

--- 04_vars/scripts.js
+++ 04_vars/scripts.js
@@ -1,8 +1,8 @@
 var content = "";
 
-content += "<h1> this is a heading </h1>;
+content += "<h1> this is a heading </h1>";
 
-content += "<p> stuff stuff stuff </p>;
+content += "<p> stuff stuff stuff </p>";
 
 document.write(content);
 
